b9f7bd8354178ab5150d269d1342dbd83b4a2f1b
Jasper Van Proeyen <trianglejuice@mac.com>
2015-04-24 10:02:50 +0200

    Release v1.0.0

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,7 +22,7 @@ module.exports = function (grunt) {
     dist: 'dist',
     version: currentversion,
     module: require('./package.json').name,
-    domain: 'tink.skeleton'
+    domain: 'tink.identitycardnumber'
   };
 
   // Define the configuration for all the tasks
--- test/spec/tink-identity-card-number-angular.js
+++ test/spec/tink-identity-card-number-angular.js
@@ -1,5 +1,5 @@
 'use strict';
-describe('tink-skeleton-angular', function() {
+describe('tink-identity-card-number-angular', function() {
 
   var bodyEl = $('body'), sandboxEl;
   var $compile, $templateCache, scope;
